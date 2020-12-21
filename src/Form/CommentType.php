6fbe40649a595594fe4647e07061620f16c40719
JulieRebeyrolle <julie.rebeyrolle@gmail.com>
2020-12-21 17:48:30 +0100

    Symfony19 challenge done

---
+++ src/Form/CommentType.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Comment;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CommentType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('comment', TextType::class, [
+                'label' => 'Votre commentaire',
+
+            ])
+            ->add('rate', ChoiceType::class, [
+                'label' => 'Votre note',
+                'label_attr' => ['class' => 'radio-custom form-check-inline'],
+
+                'choices' => [
+                    '1' => 1,
+                    '2' => 2,
+                    '3' => 3,
+                    '4' => 4,
+                    '5' => 5,
+                ],
+                'expanded' => true,
+                'multiple' => false,
+                ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Comment::class,
+        ]);
+    }
+}
