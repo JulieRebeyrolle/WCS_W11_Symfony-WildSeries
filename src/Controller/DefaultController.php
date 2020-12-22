9aa6a255e405034fb6b4aab001edc03a3199a5fc
JulieRebeyrolle <julie.rebeyrolle@gmail.com>
2020-12-22 17:37:31 +0100

    Symfony21 challenge done

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Program;
+use App\Repository\ProgramRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -10,12 +12,17 @@ class DefaultController extends AbstractController
 {
     /**
      * @Route("/", name="app_index")
+     * @param ProgramRepository $programRepository
+     * @return Response
      */
 
-    public function index(): Response
+    public function index(ProgramRepository $programRepository): Response
     {
+        $programs = $programRepository->findBy([], ['id' => 'DESC'], 3);
+
         return $this->render('index.html.twig', [
             'website' => 'Wild Séries',
+            'programs' => $programs,
         ]);
     }
 }
---
+++ src/Controller/UserController.php
@@ -0,0 +1,28 @@
+<?php
+
+
+namespace App\Controller;
+
+
+use App\Entity\User;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class UserController extends \Symfony\Bundle\FrameworkBundle\Controller\AbstractController
+{
+
+    /**
+     * @Route("/mon-compte", name="my-profile")
+     * @return Response
+     */
+    public function showProfile() : Response
+    {
+        $user = $this->getUser();
+
+        return $this->render('user/profile.html.twig', [
+            'user' => $user,
+        ]);
+
+    }
+
+}
