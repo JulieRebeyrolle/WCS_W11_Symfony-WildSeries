4225d96ada0053a05bba6b1c4935c88c661e4248
JulieRebeyrolle <julie.rebeyrolle@gmail.com>
2021-01-03 15:45:11 +0100

    add actors crud

--- src/Controller/ActorController.php
+++ src/Controller/ActorController.php
@@ -1,61 +1,109 @@
 <?php
 
-
 namespace App\Controller;
 
-
 use App\Entity\Actor;
-use App\Entity\Program;
+use App\Form\ActorType;
+use App\Repository\ActorRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
- * Class ActorController
- * @package App\Controller
- * @Route ({
+ * @Route({
  *     "en": "/actors",
  *     "fr": "/acteurs"
- * }, name="actor_")
+ * })
  */
-
 class ActorController extends AbstractController
 {
-
     /**
-     * @Route("/", name="index")
+     * @Route("/", name="actor_index", methods={"GET"})
+     * @param ActorRepository $actorRepository
+     * @return Response
      */
-
-    public function index(): Response
+    public function index(ActorRepository $actorRepository): Response
     {
-        $actors = $this->getDoctrine()
-            ->getRepository(Actor::class)
-            ->findAll();
         return $this->render('actor/index.html.twig', [
-            'actors' => $actors,
+            'actors' => $actorRepository->findAll(),
         ]);
     }
 
     /**
-     * @Route("/{id}", name="show")
-     * @param string $id
+     * @Route("/new", name="actor_new", methods={"GET","POST"})
+     * @param Request $request
      * @return Response
      */
-
-    public function show(string $id) :Response
+    public function new(Request $request): Response
     {
-        $actor = $this->getDoctrine()
-            ->getRepository(Actor::class)
-            ->findOneBy(['id' => $id]);
+        $actor = new Actor();
+        $form = $this->createForm(ActorType::class, $actor);
+        $form->handleRequest($request);
 
-        if (!$actor) {
-            throw $this->createNotFoundException(
-                'Acteur non trouvé dans notre base de données'
-            );
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($actor);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('actor_index');
         }
 
-        return $this->render("actor/show.html.twig", [
+        return $this->render('actor/new.html.twig', [
+            'actor' => $actor,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="actor_show", methods={"GET"})
+     * @param Actor $actor
+     * @return Response
+     */
+    public function show(Actor $actor): Response
+    {
+        return $this->render('actor/show.html.twig', [
             'actor' => $actor,
         ]);
+    }
+
+    /**
+     * @Route("/{id}/edit", name="actor_edit", methods={"GET","POST"})
+     * @param Request $request
+     * @param Actor $actor
+     * @return Response
+     */
+    public function edit(Request $request, Actor $actor): Response
+    {
+        $form = $this->createForm(ActorType::class, $actor);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('actor_index');
+        }
+
+        return $this->render('actor/edit.html.twig', [
+            'actor' => $actor,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="actor_delete", methods={"DELETE"})
+     * @param Request $request
+     * @param Actor $actor
+     * @return Response
+     */
+    public function delete(Request $request, Actor $actor): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$actor->getId(), $request->request->get('_token'))) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->remove($actor);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('actor_index');
     }
 }
---
+++ src/Form/ActorType.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Actor;
+use App\Entity\Program;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Contracts\Translation\TranslatorInterface;
+
+class ActorType extends AbstractType
+{
+
+    public $translator;
+    public function __construct(TranslatorInterface $translator)
+    {
+        $this->translator = $translator;
+    }
+
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Actor::class,
+        ]);
+    }
+}
